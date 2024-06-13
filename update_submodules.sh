bc029bad37fe1560b3c1aa1eea93104f8d5d81c8
nunzioono <nunzioonorati@gmail.com>
2024-06-13 19:27:28 +0200

    Made the same script for updating submodules for macos/linux shells

--- update_submodules.sh
+++ update_submodules.sh
@@ -1,4 +1,4 @@
-# Update all submodules to the latest commit on their respective branches
+#!/bin/bash
 git submodule foreach 'git checkout main && git pull origin main'
 git add .
 git commit -m "Updated submodules to latest commit on main"
